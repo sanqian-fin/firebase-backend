3e11d1b556d1304e55d1423fc75ce543b2393de1
ak1103dev <ak.apichan1103@gmail.com>
2019-06-15 22:54:35 +0700

    :construction: init first time

---
+++ functions/constants/categories.js
@@ -0,0 +1,94 @@
+module.exports = [{
+  id: 'Food',
+  name: 'Food',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Transportation',
+  name: 'Transportation',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'InternetPhone',
+  name: 'Internet & Phone',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'LearningBook',
+  name: 'Learning & Book',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'SocialSecurity',
+  name: 'Social Security',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Tax',
+  name: 'Tax',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'PartyTrip',
+  name: 'Party & Trip',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Fee',
+  name: 'Fee',
+  icon: 'FaQuestionCircle',
+}, {
+}, {
+  id: 'Donate',
+  name: 'Donate',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Lottery',
+  name: 'Lottery',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Stock',
+  name: 'Stock',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'MutualFund',
+  name: 'Mutual Fund',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'RMF',
+  name: 'RMF',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'LTF',
+  name: 'LTF',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'CryptoCurrency',
+  name: 'Crypto Currency',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Valuable Thing',
+  name: 'Valuable Thing',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Salary',
+  name: 'Salary',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Wage',
+  name: 'Wage',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Bonus',
+  name: 'Bonus',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Interest',
+  name: 'Interest',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Dividend',
+  name: 'Dividend',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Borrowing',
+  name: 'Borrowing',
+  icon: 'FaQuestionCircle',
+}, {
+  id: 'Other',
+  name: 'Other',
+  icon: 'FaQuestionCircle',
+}]
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,45 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+const categories = require('./constants/categories')
+
+admin.initializeApp();
+
+// exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+
 // });
+
+exports.initialCategories = functions.auth.user().onCreate(async (user) => {
+  try {
+    await Promise.all(
+      categories.map((item) => {
+        const data = {
+          ...item,
+          id: undefined,
+          userId: user.data.uid
+        }
+        return admin.firestore().collection('categories').doc(item.id).set(data)
+      })
+    )
+    console.log('Initial Categories Success')
+  } catch (err) {
+    console.log(err)
+    return
+  }
+});
+
+exports.createFirstAccount = functions.auth.user().onCreate(async (user) => {
+  const userDoc = {
+    email: user.data.email,
+  }
+  try {
+    const result  = await admin.firestore()
+      .collection('users')
+      .doc(user.data.uid)
+      .set(userDoc)
+    console.log('User Created result:', result)
+  } catch(err) {
+    console.log(err)
+    return
+  }
+});
